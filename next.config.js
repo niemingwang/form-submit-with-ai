e84eb41c9ab5aafe479ee7bc38fe39e958ad6479
niemingwang <1984607221@qq.com>
2025-03-17 20:37:44 +0800

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/form-submit-with-ai",
   reactStrictMode: true,
   async rewrites() {
     return [
